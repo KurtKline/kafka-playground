49624c8a52de80d500574fd5a4100d8f0b1d3c02
Kurt Kline <kurtm.kline@gmail.com>
2020-06-07 16:13:41 +0200

    Adding 2 additional consumers and data.

---
+++ busdata2.py
@@ -0,0 +1,45 @@
+import json
+import uuid
+from datetime import datetime
+from time import sleep
+from typing import List
+
+from pykafka import KafkaClient
+
+
+with open("data/bus2.json", "r") as bus_data:
+    data = json.load(bus_data)
+    coordinates = data["features"][0]["geometry"]["coordinates"]
+
+
+def generate_uuid():
+    return uuid.uuid4()
+
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics["testBusData"]
+producer = topic.get_sync_producer()
+
+data = {}
+data["busline"] = "00002"
+
+
+def generate_checkpoint(coordinates: List[List[str]]):
+
+    i = 0
+    while i < len(coordinates):
+        data["key"] = data["busline"] + "_" + str(generate_uuid())
+        data["timestamp"] = str(datetime.utcnow())
+        data["latitude"] = coordinates[i][1]
+        data["longitude"] = coordinates[i][0]
+        message = json.dumps(data)
+        print(message)
+        producer.produce(message.encode("ascii"))
+
+        if i == len(coordinates) - 1:
+            i = 0
+        else:
+            i += 1
+
+
+generate_checkpoint(coordinates)
