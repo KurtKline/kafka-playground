7a3f52162716cf52a6b14531c42540d3c09c6f31
Kurt Kline <kurtm.kline@gmail.com>
2020-06-07 14:50:12 +0200

    pre-commit on all files

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import json
-from time import sleep
-from datetime import datetime
 import uuid
+from datetime import datetime
+from time import sleep
+
 from pykafka import KafkaClient
 
 
-with open('data/bus1.json', 'r') as bus_data:
+with open("data/bus1.json", "r") as bus_data:
     data = json.load(bus_data)
-    coords = data['features'][0]['geometry']['coordinates']
+    coords = data["features"][0]["geometry"]["coordinates"]
 
 
 client = KafkaClient(hosts="localhost:9092")
-topic = client.topics['testBusData']
+topic = client.topics["testBusData"]
 
 producer = topic.get_sync_producer()
 
-for x, y in coords: 
-    producer.produce(f'X: {x}, Y: {y}'.encode('ascii'))
+for x, y in coords:
+    producer.produce(f"X: {x}, Y: {y}".encode("ascii"))
     sleep(1)
